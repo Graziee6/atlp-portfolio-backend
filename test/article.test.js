3636b956c0853d11028076586ea5a350989b7fd6
Graziee6 <gngrace10@gmail.com>
2022-05-09 21:00:07 +0200

    fix: rename test files as required

--- test/article.test.js
+++ test/article.test.js
@@ -1,6 +1,6 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const server = require("./../index");
+const server = require("../index");
 
 //Assertion style
 chai.should();
